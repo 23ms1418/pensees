a3feb3272c8c93bad82a0f3a794766420ed1b1a5
ms202218 <23ms1418@h.jne.go.kr>
2023-11-22 14:53:41 +0900

    data api test

--- school_calendar/urls.py
+++ school_calendar/urls.py
@@ -3,4 +3,5 @@ from school_calendar import views
 
 urlpatterns = [
     path("", views.index),
+    path("api/get_data", views.get_data)
 ]
--- school_calendar/views.py
+++ school_calendar/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def index(req):
     return render(req, "school_calendar.html")
+
+def get_data(request):
+    data = {'message': 'Hello from Django backend!'}
+    return JsonResponse(data)
